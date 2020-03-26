867f9b4ee8e5455cb62fc046c757de0d46bb4a95
Zach Fedor <zachfedor@gmail.com>
2020-03-26 14:18:53 -0400

    Add greeting route and form on homepage for username

--- flaskdemo/__init__.py
+++ flaskdemo/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 def create_app(test_config=None):
     app = Flask(__name__)
@@ -11,13 +11,30 @@ def create_app(test_config=None):
     else:
         app.config.from_mapping(test_config)
 
-    @app.route('/')
+    @app.route('/', methods=('GET', 'POST'))
     def index():
-        return render_template('homepage.html')
+        if (request.method == 'POST'):
+            # form was sent
+            name = request.form['user_name']
+
+            if name == "":
+                return render_template('homepage.html', error="Sorry, but you didn't enter a name")
+            elif 'E' in name:
+                # this is bad, you can't have an 'E' in your name dingus!
+                return redirect(url_for('greet', name="Dingus"))
+            else:
+                return render_template('homepage.html', name=name)
+        else:
+            # first view of page
+            return render_template('homepage.html')
 
     @app.route('/about')
     def about():
         return render_template('about.html')
+
+    @app.route('/greet/<name>')
+    def greet(name):
+        return render_template('greet.html', person=name)
 
     return app
 
--- tests/test_flaskdemo.py
+++ tests/test_flaskdemo.py
@@ -18,3 +18,12 @@ def test_about_page(client):
     response = client.get('/about')
     assert b'This is a quick demo application' in response.data
 
+
+def test_greeting_page(client):
+    response1 = client.get('/greet/Amy')
+    assert b'Hello Amy' in response1.data
+
+    response2 = client.get('/greet/Bob')
+    assert b'Hello Amy' not in response2.data
+    assert b'Hello Bob' in response2.data
+
